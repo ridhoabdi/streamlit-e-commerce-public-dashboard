808855e47bf9269df3c512ee9770ea9a9afd4027
ridhoabdi <ridhoabdi101@gmail.com>
2023-09-20 12:59:35 +0700

    upload files notebook.ipynb , all_data.csv

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,1 +1,144 @@
-print("hello world")
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+
+sns.set(style='dark')
+
+# Helper function yang dibutuhkan untuk menyiapkan berbagai dataframe
+
+def create_daily_orders_df(df):
+    daily_orders_df = df.resample(rule='D', on='order_approved_at').agg({
+        "order_id": "nunique",
+        "payment_value": "sum"
+    })
+    daily_orders_df = daily_orders_df.reset_index()
+    daily_orders_df.rename(columns={
+        "order_id": "order_count",
+        "payment_value": "revenue"
+    }, inplace=True)
+    
+    return daily_orders_df
+
+def create_sum_spend_df(df):
+    sum_spend_df = df.resample(rule='D', on='order_approved_at').agg({
+        "payment_value": "sum"
+    })
+    sum_spend_df = sum_spend_df.reset_index()
+    sum_spend_df.rename(columns={
+        "payment_value": "total_spend"
+    }, inplace=True)
+
+    return sum_spend_df
+
+def create_sum_order_items_df(df):
+    sum_order_items_df = df.groupby("product_category_name_english")["product_id"].count().reset_index()
+    sum_order_items_df.rename(columns={
+        "product_id": "product_count"
+    }, inplace=True)
+    sum_order_items_df = sum_order_items_df.sort_values(by='product_count', ascending=False)
+
+    return sum_order_items_df
+
+def create_bystate_df(df):
+    bystate_df = df.groupby(by="customer_state").customer_id.nunique().reset_index()
+    bystate_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    most_common_state = bystate_df.loc[bystate_df['customer_count'].idxmax(), 'customer_state']
+    bystate_df = bystate_df.sort_values(by='customer_count', ascending=False)
+
+    return bystate_df, most_common_state
+
+# def create_bycity_df(df):
+#     bycity_df = df.groupby(by="customer_city").customer_id.nunique().reset_index()
+#     bycity_df.rename(columns={
+#         "customer_id": "customer_count"
+#     }, inplace=True)
+#     most_common_city = bycity_df.loc[bycity_df['customer_count'].idxmax(), 'customer_city']
+#     bycity_df = bycity_df.sort_values(by='customer_count', ascending=False)
+
+#     return bycity_df, most_common_city
+
+def create_rfm_df(df):
+    rfm_df = df.groupby(by="customer_id", as_index=False).agg({
+        "order_purchase_timestamp": "max",  # Mengambil tanggal order terakhir
+        "order_id": "nunique",
+        "payment_value": "sum"  # Menghitung total nilai pembayaran
+    })
+    rfm_df.columns = ["customer_id", "max_purchase_timestamp", "frequency", "monetary"]
+    
+    rfm_df["max_purchase_timestamp"] = rfm_df["max_purchase_timestamp"].dt.date
+    recent_date = df["purchase_date"].dt.date.max()
+    rfm_df["recency"] = rfm_df["max_purchase_timestamp"].apply(lambda x: (recent_date - x).days)
+    rfm_df.drop("max_purchase_timestamp", axis=1, inplace=True)
+    
+    return rfm_df
+
+# Load cleaned data
+all_df = pd.read_csv("./submission-analisis-data-python/dashboard/all_data.csv")
+datetime_columns = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
+all_df.sort_values(by="order_approved_at", inplace=True)
+all_df.reset_index(inplace=True)
+
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
+
+# Filter data
+min_date = all_df["order_approved_at"].min()
+max_date = all_df["order_approved_at"].max()
+
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) & 
+                 (all_df["order_approved_at"] <= str(end_date))]
+
+# Menyiapkan berbagai dataframe
+daily_orders_df = function.create_daily_orders_df()
+sum_spend_df = function.create_sum_spend_df()
+sum_order_items_df = function.create_sum_order_items_df()
+review_score, common_score = function.review_score_df()
+state, most_common_state = function.create_bystate_df()
+# city, most_common_city = function.create_bycity_df()
+order_status, common_status = function.create_order_status()
+
+# plot number of daily orders
+st.header('E-Commerce Dashboard :sparkles:')
+st.subheader('Daily Orders')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    total_orders = daily_orders_df.order_count.sum()
+    st.metric("Total orders", value=total_orders)
+
+with col2:
+    total_revenue = format_currency(daily_orders_df.revenue.sum(), "AUD", locale='es_CO') 
+    st.metric("Total Revenue", value=total_revenue)
+
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    daily_orders_df["order_approved_at"],
+    daily_orders_df["order_count"],
+    marker="o",
+    linewidth=2,
+    color="#90CAF9"
+)
+
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+
+st.pyplot(fig)
+
+
+st.caption('Copyright © Muhammad Ridho Abdillah 2023')
